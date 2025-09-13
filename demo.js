2987df9e11e2be782b7f753c334c7e4ae6a7acc6
Thanh Thien <thien100305@gmail.com>
2025-09-13 17:11:01 +0700

    Chỉnh sửa hàm showInfo

--- demo.js
+++ demo.js
@@ -1,6 +1,10 @@
-function showInfo(name, age) {
+function showInfo(name, birthYear) {
+    const currentYear = new Date().getFullYear();
+    const age = currentYear - birthYear;
+
     console.log(`Họ và tên: ${name}`);
+    console.log(`Năm sinh: ${birthYear}`);
     console.log(`Tuổi: ${age}`);
 }
 
-showInfo("Lê Thanh Thiên", 20);
+showInfo("Lê Thanh Thiên", 2005);
